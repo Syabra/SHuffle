0cd07fe7e077b584c17e4cd1327b27f1d3da5dc2
Yauheny <Syabra.bel@gmail.com>
2019-06-24 22:10:41 +0300

    Work program

---
+++ ShuffleGame/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace ShuffleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(9, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(71, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(133, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(194, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 51);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(9, 119);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 51);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(71, 119);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 51);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(133, 119);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 51);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(194, 119);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(65, 51);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(9, 164);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(65, 51);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(71, 164);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(65, 51);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(133, 164);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(65, 51);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button12.Location = new System.Drawing.Point(194, 164);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(65, 51);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button13.Location = new System.Drawing.Point(9, 212);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(65, 51);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "13";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button14.Location = new System.Drawing.Point(71, 212);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(65, 51);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "14";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button15.Location = new System.Drawing.Point(133, 212);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(65, 51);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "15";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button16.Location = new System.Drawing.Point(194, 212);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(65, 51);
+            this.button16.TabIndex = 15;
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(204, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(152, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Clicks :";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(272, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.NewGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 278);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ShuffleGame/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShuffleGame
+{
+    public partial class Form1 : Form
+    {
+        int num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CheckButton(Button bttn1, Button bttn2)
+        {
+            if (bttn2.Text == "")
+            {
+                bttn2.Text = bttn1.Text;
+                bttn1.Text = "";
+            }
+        }
+
+        public void CheckSolve()
+        {
+            num = num + 1;
+            label1.Text = num + " ";
+            if (button1.Text=="1"&& button2.Text == "2" && button3.Text == "3" && button4.Text == "4" &&
+                button5.Text == "5" && button6.Text == "6" && button7.Text == "7" && button8.Text == "8" &&
+                button9.Text == "9" && button10.Text == "10" && button11.Text == "11" && button12.Text == "12" &&
+                button13.Text == "13" && button14.Text == "14" && button15.Text == "15" && button16.Text == "")
+            {
+                MessageBox.Show("You did it in" + num + " click");
+            }
+        }   
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CheckButton(button1, button2);
+            CheckButton(button1, button5);
+            CheckSolve();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            CheckButton(button2, button1);
+            CheckButton(button2, button3);
+            CheckButton(button2, button6);
+            CheckSolve();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            CheckButton(button3, button2);
+            CheckButton(button3, button4);
+            CheckButton(button3, button7);
+            CheckSolve();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            CheckButton(button4, button3);
+            CheckButton(button4, button8);
+            CheckSolve();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            CheckButton(button5, button1);
+            CheckButton(button5, button6);
+            CheckButton(button5, button9);
+            CheckSolve();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            CheckButton(button6, button5);
+            CheckButton(button6, button2);
+            CheckButton(button6, button7);
+            CheckButton(button6, button10);
+            CheckSolve();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            CheckButton(button7, button6);
+            CheckButton(button7, button3);
+            CheckButton(button7, button8);
+            CheckButton(button7, button11);
+            CheckSolve();
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            CheckButton(button8, button7);
+            CheckButton(button8, button4);
+            CheckButton(button8, button12);
+            CheckSolve();
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            CheckButton(button9, button5);
+            CheckButton(button9, button10);
+            CheckButton(button9, button13);
+            CheckSolve();
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            CheckButton(button10, button9);
+            CheckButton(button10, button6);
+            CheckButton(button10, button11);
+            CheckButton(button10, button14);
+            CheckSolve();
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            CheckButton(button11, button10);
+            CheckButton(button11, button7);
+            CheckButton(button11, button12);
+            CheckButton(button11, button15);
+            CheckSolve();
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            CheckButton(button12, button8);
+            CheckButton(button12, button11);
+            CheckButton(button12, button16);
+            CheckSolve();
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            CheckButton(button13, button9);
+            CheckButton(button13, button14);
+            CheckSolve();
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            CheckButton(button14, button13);
+            CheckButton(button14, button10);
+            CheckButton(button14, button15);
+            CheckSolve();
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            CheckButton(button15, button14);
+            CheckButton(button15, button11);
+            CheckButton(button15, button16);
+            CheckSolve();
+        }
+
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            CheckButton(button16, button12);
+            CheckButton(button16, button15);
+            CheckSolve();
+        }
+
+        public void Shuffle()
+        {
+            int i, j, Rn;
+            int[] a = new int[16];
+            Boolean Flags = false;
+            i = 1;
+            do
+            {
+                Random rnd = new Random();
+                Rn = Convert.ToInt32(rnd.Next(0, 15) + 1);
+                for (j = 1; j <= i; j++)
+                {
+                    if (a[j] == Rn)
+                    {
+                        Flags = true;
+                        break;   
+                    }
+                }
+                if (Flags == true)
+                {
+                    Flags = false;
+                }
+                else
+                {
+                    a[i] = Rn;
+                    i = i + 1;
+
+                }
+            }
+            while (i <= 15);
+            button1.Text = Convert.ToString(a[1]);
+            button2.Text = Convert.ToString(a[2]);
+            button3.Text = Convert.ToString(a[3]);
+            button4.Text = Convert.ToString(a[4]);
+            button5.Text = Convert.ToString(a[5]);
+            button6.Text = Convert.ToString(a[6]);
+            button7.Text = Convert.ToString(a[7]);
+            button8.Text = Convert.ToString(a[8]);
+            button9.Text = Convert.ToString(a[9]);
+            button10.Text = Convert.ToString(a[10]);
+            button11.Text = Convert.ToString(a[11]);
+            button12.Text = Convert.ToString(a[12]);
+            button13.Text = Convert.ToString(a[13]);
+            button14.Text = Convert.ToString(a[14]);
+            button15.Text = Convert.ToString(a[15]);
+            button16.Text = "";
+            num = 0;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
